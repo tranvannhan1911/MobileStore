376a6f6a9fc6e59566a22d8ad14888cdb6775cd5
tdat3023 <83853456+tdat3023@users.noreply.github.com>
2021-05-24 09:53:02 +0700

    Add files via upload

---
+++ js/dangnhap.js
@@ -0,0 +1,12 @@
+let container = document.getElementById('container');
+
+toggle  = () => {
+    container.classList.toggle('sign-in')
+    container.classList.toggle('sign-up')
+}
+
+setTimeout(() => {
+
+    container.classList.add('sign-in')
+
+},200)
